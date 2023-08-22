692b605d676765cda978ac57fd1c746ca1b18be6
francotaind <francotaind00@gmail.com>
2023-08-22 08:36:00 +0300

    first correction

--- main.h
+++ main.h
@@ -77,12 +77,6 @@ int print_non_printable(va_list types, char buffer[],
 /* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_char(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);

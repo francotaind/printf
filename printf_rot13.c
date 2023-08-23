08de84fe3c803e2bed3b4bd162bdca80fd7dbfe5
francotaind <francotaind00@gmail.com>
2023-08-22 21:57:38 -0700

    corrections

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * printf_rot13 - printf str to ROT13 place into buffer
+ * printf_rot13 - prints string to ROT13 place into buffer
  * @args: type struct va_arg where is allocated printf arguments
  * Return: counter
- *
  */
 int printf_rot13(va_list args)
 {
